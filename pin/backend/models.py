c64d898a578428dc6effe59e975c8640a21ce14b
Marcos <sockramx@gmail.com>
2019-04-03 08:46:33 -0500

    Creado modelo Pin y agredado al admin

--- pin/backend/models.py
+++ pin/backend/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Pin(models.Model):
+    image = models.CharField(max_length=200)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=180)
 
